5dc5e7074ad4e36464e47435e8fc78e642891ff2
Diana L Duran <dduran.andrade@gmail.com>
2023-12-27 15:00:07 -0600

    using split method and format for text processing

---
+++ src/main/java/strings/TextProcessor.java
@@ -0,0 +1,20 @@
+package strings;
+
+public class TextProcessor {
+    public static void main(String[] args) {
+        countWords("I love Java");
+    }
+
+    private static void countWords(String text) {
+        var words = text.split(" ");
+        int numberOfWords = words.length;
+
+        String message = String.format("Your text contains %d words", numberOfWords);
+        System.out.println(message);
+
+        for(String word : words){
+            System.out.println(word);
+        }
+    }
+
+}
