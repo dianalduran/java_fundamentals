04ad0eb82780a3ba22181a6e7298f09d42f879ad
Diana L Duran <dduran.andrade@gmail.com>
2023-12-27 15:17:15 -0600

    using StringBuilder to modify text

--- src/main/java/strings/TextProcessor.java
+++ src/main/java/strings/TextProcessor.java
@@ -3,7 +3,8 @@ package strings;
 public class TextProcessor {
     public static void main(String[] args) {
         //countWords("I love Java");
-        reverseString("Hello World");
+        //reverseString("Hello World");
+        addSpaces("HeyWorldIt'sMeDiana");
     }
 
     public static void countWords(String text) {
@@ -22,6 +23,17 @@ public class TextProcessor {
         for(int i = text.length()-1; i >= 0; i--){
             System.out.print(text.charAt(i));
         }
+    }
+
+    public static void addSpaces(String text){
+        var modifiedText = new StringBuilder(text);
+        for (int i = 0; i < modifiedText.length(); i++){
+            if(i != 0 && Character.isUpperCase(modifiedText.charAt(i))){
+                modifiedText.insert(i, " ");
+                i++;
+            }
+        }
+        System.out.println(modifiedText);
     }
 
 }
