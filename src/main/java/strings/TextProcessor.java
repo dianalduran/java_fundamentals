3f2adab347a9634f2ef3d9235a81503b3096fe8d
Diana L Duran <dduran.andrade@gmail.com>
2023-12-27 15:07:31 -0600

    reverse string processing

--- src/main/java/strings/TextProcessor.java
+++ src/main/java/strings/TextProcessor.java
@@ -2,10 +2,11 @@ package strings;
 
 public class TextProcessor {
     public static void main(String[] args) {
-        countWords("I love Java");
+        //countWords("I love Java");
+        reverseString("Hello World");
     }
 
-    private static void countWords(String text) {
+    public static void countWords(String text) {
         var words = text.split(" ");
         int numberOfWords = words.length;
 
@@ -14,6 +15,12 @@ public class TextProcessor {
 
         for(String word : words){
             System.out.println(word);
+        }
+    }
+
+    public static void reverseString(String text) {
+        for(int i = text.length()-1; i >= 0; i--){
+            System.out.print(text.charAt(i));
         }
     }
 
