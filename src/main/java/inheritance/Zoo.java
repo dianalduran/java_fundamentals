36d668ab46acf5ba0773233e9fb715ed515299b6
Diana L Duran <dduran.andrade@gmail.com>
2023-12-29 02:19:51 -0600

    used instanceof operator

--- src/main/java/inheritance/Zoo.java
+++ src/main/java/inheritance/Zoo.java
@@ -5,13 +5,25 @@ public class Zoo {
         Dog rocky = new Dog();
         //rocky.fetch();
         //rocky.makeSound();
+        feed(rocky);
 
         Animal sasha = new Dog();
         //sasha.makeSound();
+        feed(sasha);
 
         sasha = new Cat();
-        sasha.makeSound();
-        ((Cat)sasha).scratch();
+        //sasha.makeSound();
+        //((Cat)sasha).scratch();
+        feed(sasha);
 
+    }
+
+    public static void feed(Animal animal){
+        if(animal instanceof Dog){
+            System.out.println("Here's your dog food");
+        }
+        else if (animal instanceof Cat){
+            System.out.println("Here's your cat food");
+        }
     }
 }
