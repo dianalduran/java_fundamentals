23259c09fe4cd992dcdac50b35d8687a475c53ac
Diana L Duran <dduran.andrade@gmail.com>
2023-12-28 20:57:04 -0600

    overriding parameters of inherited classes

--- src/main/java/inheritance/InheritanceTester.java
+++ src/main/java/inheritance/InheritanceTester.java
@@ -3,6 +3,10 @@ package inheritance;
 public class InheritanceTester {
     public static void main(String[] args){
         //Person person = new Person();
-        Employee employee = new Employee();
+        //Employee employee = new Employee();
+
+        Square square = new Square();
+        square.setLength(25);
+        System.out.println(square.calculatePerimeter());
     }
 }
