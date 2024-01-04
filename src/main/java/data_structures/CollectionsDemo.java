235b852d4309a7378595639f9842ce9ae7b4d6dc
Diana L Duran <dduran.andrade@gmail.com>
2024-01-04 10:32:15 -0600

    using the List data structure

--- src/main/java/data_structures/CollectionsDemo.java
+++ src/main/java/data_structures/CollectionsDemo.java
@@ -1,11 +1,14 @@
 package data_structures;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class CollectionsDemo {
     public static void main(String[] args){
-        setDemo();
+//        setDemo();
+        listDemo();
     }
 
     public static void setDemo(){
@@ -22,5 +25,25 @@ public class CollectionsDemo {
 
 //        Set.of is an immutable set
         Set moreFruits = Set.of("pear", "cherry", "raisin");
+    }
+
+    public static void listDemo(){
+        List fruits = new ArrayList();
+        fruits.add("apple");
+        fruits.add("lemon");
+        fruits.add("banana");
+        fruits.add("orange");
+
+        fruits.set(2, "grape");
+        fruits.add("lemon");
+//        fruits.remove("lemon"); //removes the first found
+//        fruits.remove(3);
+
+        System.out.println("index 2: " + fruits.get(2));
+        System.out.println("index of grape: " + fruits.indexOf("grape"));
+        System.out.println("last index of lemon: " + fruits.lastIndexOf("lemon"));
+        System.out.println(fruits);
+
+        List moreFruit = List.of("cherry", "plum"); //immutable
     }
 }
