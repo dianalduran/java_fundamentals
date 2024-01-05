49e548f8cd04ca4ad396298800d1c29e407e0b69
Diana L Duran <dduran.andrade@gmail.com>
2024-01-05 01:29:37 -0600

    closed scanner

--- src/main/java/exceptions/MultipleExceptionHandling.java
+++ src/main/java/exceptions/MultipleExceptionHandling.java
@@ -8,13 +8,16 @@ import java.util.Scanner;
 public class MultipleExceptionHandling {
     public static void main(String[] args){
         File file = new File("files/numbers.txt");
+        Scanner fileReader = null;
         try {
-            Scanner fileReader = new Scanner(file);
+            fileReader = new Scanner(file);
             while(fileReader.hasNext()){
                 System.out.println(fileReader.nextDouble());
             }
         } catch(FileNotFoundException | InputMismatchException e){
             System.out.println(e.getMessage());
+        } finally {
+            fileReader.close();
         }
 
     }
