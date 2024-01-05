ca2c47b9db0489132d4d42ea51197dd4d0954f2a
Diana L Duran <dduran.andrade@gmail.com>
2024-01-05 06:41:57 -0600

    using PrintWriter to print to file

--- src/main/java/exceptions/TryWithResources.java
+++ src/main/java/exceptions/TryWithResources.java
@@ -2,15 +2,20 @@ package exceptions;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class TryWithResources {
     public static void main(String[] args){
-        File file = new File("files/numbers.txt");
-        try(Scanner fileReader = new Scanner(file)) {
+        File inputFile = new File("files/numbers.txt");
+        File outputFile = new File("files/output.txt");
+        try(
+                Scanner fileReader = new Scanner(inputFile);
+                PrintWriter fileWriter = new PrintWriter(outputFile)
+        ) {
             while(fileReader.hasNext()){
-                System.out.println(fileReader.nextDouble());
+                fileWriter.println(fileReader.nextDouble());
             }
         } catch(FileNotFoundException | InputMismatchException e){
             System.out.println(e.getMessage());
